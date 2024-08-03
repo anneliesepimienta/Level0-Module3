9d35b93c9baab539fe3bf57b2f5b13f6ca07596a
league <yayalisa91@gmail.com>
2024-08-03 12:55:50 -0700

    code of august 3/2024

--- _01_elif/_3_are_you_happy/are_you_happy.py
+++ _01_elif/_3_are_you_happy/are_you_happy.py
@@ -1,8 +1,21 @@
 from tkinter import Tk, simpledialog, messagebox
 
-
 if __name__ == '__main__':
     # TODO: Look at the AreYouHappy.png image
     #       Use pop-ups to recreate the chart using if and elif statements
+    window = Tk()
+    window.withdraw()
+    happiness_meter = simpledialog.askstring(title="", prompt="Are you happy? yes or no?")
+    if happiness_meter == "yes":
+        messagebox.showinfo(title="", message="keep doing whatever your doing")
+    elif happiness_meter == "no":
+        happiness_meter_2 = simpledialog.askstring(title="", prompt="Do you want to be happy? yes or no?")
 
-    pass
+        if happiness_meter_2 == "yes":
+            messagebox.showinfo(title="", message="Change something")
+        elif happiness_meter_2 == "no":
+            messagebox.showinfo(title="", message="Keep doing whatever your doing")
+        else:
+            messagebox.showinfo(title="", message="there was an error in your text. Please exit out and try again")
+    else:
+        messagebox.showinfo(title="", message="there was an error in your text. please exit out and try again")
--- _01_elif/_4_own_adventure/own_adventure.py
+++ _01_elif/_4_own_adventure/own_adventure.py
@@ -4,3 +4,29 @@ from tkinter import simpledialog, messagebox, Tk
 #  path of the plot.
 #  Use pop-ups, if statements, and your imagination to make an interesting
 #  story
+window = Tk()
+window.withdraw()
+
+if __name__ == '__main__':
+
+    story = simpledialog.askstring(title="", prompt = "wanna hear a story?")
+    if story == "yes":
+        story_1 = simpledialog.askstring(title="", prompt="okeydokey! here we go............................there was a cat...what do you think the cat's name should be? Bob, pesky, or, yada?")
+        if story_1 == "Bob":
+            messagebox.showinfo(title="", message="okeydokey, bob it is!")
+        elif story_1 == "pesky":
+            messagebox.showinfo(title="", message="okeydokey, pesky it is!")
+        elif story_1 == "yada":
+            messagebox.showinfo(title="", message="okeydokey, yada it is!")
+        else:
+            messagebox.showinfo(title="", message="there was an error in your text. please exit out and try again. please be aware that the error could have just been a typo or you not writing exactly what was in the message")
+    elif story =="no":
+        story_2 = simpledialog.askstring(title="", prompt="Well sucks for you! Here we go.............................There was a cat...What do you think the cat's name should be? Bob, pesky, or yada?")
+        if story_2 == "Bob":
+            messagebox.showinfo(title="", message="okeydokey, bob it is!")
+        elif story_2 == "pesky":
+            messagebox.showinfo(title="", message="okeydokey, pesky it is!")
+        elif story_2 == "yada":
+            messagebox.showinfo(title="", message="okeydokey, yada it is!")
+        else:
+            messagebox.showinfo(title="", message="there was an error in your text. please exit out and try again. please be aware that the error could have just been a typo or you not writing exactly what was in the message")
